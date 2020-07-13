f8e83385d17f54f19836c274b8284b0820a7ee23
yalounin <46644853+yalounin@users.noreply.github.com>
2020-07-13 17:45:07 +0500

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Max Yalounin on 13.07.2020.
+//  Copyright © 2020 Max Yalounin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
